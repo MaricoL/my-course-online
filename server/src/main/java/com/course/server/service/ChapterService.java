8e25d90d6d545873c05f23e5879e14144e827684
mrjimmylin <mrjimmylin@163.com>
2021-02-21 09:43:16 +0800

    1. 在Server模块中增加 dto 层 controller和service使用dto层， service和mapper使用dao层

--- server/src/main/java/com/course/server/service/ChapterService.java
+++ server/src/main/java/com/course/server/service/ChapterService.java
@@ -2,10 +2,13 @@ package com.course.server.service;
 
 import com.course.server.domain.Chapter;
 import com.course.server.domain.ChapterExample;
+import com.course.server.dto.ChapterDto;
 import com.course.server.mapper.ChapterMapper;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -14,10 +17,18 @@ public class ChapterService {
     @Resource
     private ChapterMapper chapterMapper;
 
-    public List<Chapter> list(){
+    public List<ChapterDto> list(){
         ChapterExample example = new ChapterExample();
 //        example.createCriteria().andIdEqualTo("1");
 //        example.setOrderByClause("id desc");
-        return chapterMapper.selectByExample(example);
+        List<Chapter> chapterList = chapterMapper.selectByExample(example);
+        List<ChapterDto> chapterDtoList = new ArrayList<>();
+        for (int i = 0; i < chapterList.size(); i++) {
+            Chapter chapter = chapterList.get(i);
+            ChapterDto chapterDto = new ChapterDto();
+            BeanUtils.copyProperties(chapter, chapterDto);
+            chapterDtoList.add(chapterDto);
+        }
+        return chapterDtoList;
     }
 }
