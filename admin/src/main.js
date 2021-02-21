c180a55ba4b51a6d1273b548f7bf2d8ea3d1bf70
mrjimmylin <mrjimmylin@163.com>
2021-02-21 14:45:58 +0800

    1. 集成 axios 2. 在 chapter.vue 中发送获取数据请求 3. 在 Server模块 中添加 CorsConfig.java，解决 CORS 跨域问题

--- admin/src/main.js
+++ admin/src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './app.vue'
 import router from './router'
+import axios from 'axios'
 
 Vue.config.productionTip = false
+// 定义一个Vue全局变量，可以在任意组件中使用 this.$ajax 访问
+Vue.prototype.$ajax = axios
 
 new Vue({
   router,
