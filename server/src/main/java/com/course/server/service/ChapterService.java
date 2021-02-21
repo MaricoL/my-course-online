85fe7de3713c88dd6f07818160fe3e6e3f60e7bf
mrjimmylin <mrjimmylin@163.com>
2021-02-21 09:32:32 +0800

    1. 增加Chapter（大章）列表借口

---
+++ server/src/main/java/com/course/server/service/ChapterService.java
@@ -0,0 +1,23 @@
+package com.course.server.service;
+
+import com.course.server.domain.Chapter;
+import com.course.server.domain.ChapterExample;
+import com.course.server.mapper.ChapterMapper;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class ChapterService {
+
+    @Resource
+    private ChapterMapper chapterMapper;
+
+    public List<Chapter> list(){
+        ChapterExample example = new ChapterExample();
+//        example.createCriteria().andIdEqualTo("1");
+//        example.setOrderByClause("id desc");
+        return chapterMapper.selectByExample(example);
+    }
+}
