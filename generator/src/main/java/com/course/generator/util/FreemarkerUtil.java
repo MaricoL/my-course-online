b5622152e3ed159a9f8e2b9cec4a05f476ea63d5
mrjimmylin <mrjimmylin@163.com>
2021-02-23 21:13:52 +0800

    1. 编写 ServerGenerator.java 工具类 2. 使用 test.ftl 模板测试

---
+++ generator/src/main/java/com/course/generator/util/FreemarkerUtil.java
@@ -0,0 +1,33 @@
+package com.course.generator.util;
+
+import freemarker.template.Configuration;
+import freemarker.template.DefaultObjectWrapper;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FreemarkerUtil {
+
+    private static final String ftlPath = "generator/src/main/java/com/course/generator/ftl/";
+
+    private static Template temp;
+
+    public static void initFtl(String ftlFileName) throws IOException {
+        Configuration cfg = new Configuration(Configuration.VERSION_2_3_29);
+        cfg.setDirectoryForTemplateLoading(new File(ftlPath));
+        cfg.setObjectWrapper(new DefaultObjectWrapper(Configuration.VERSION_2_3_29));
+        temp = cfg.getTemplate(ftlFileName);
+    }
+
+    public static void generator(String fileName) throws IOException, TemplateException {
+        FileWriter fw = new FileWriter(fileName);
+        BufferedWriter bw = new BufferedWriter(fw);
+        temp.process(null, bw);
+        bw.flush();
+        fw.close();
+    }
+}
